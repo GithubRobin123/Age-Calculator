9d1fea6a7838888d1c5a9071194215dffccdd834
Robin <vishalmaaan9654@gmail.com>
2022-03-09 17:18:56 +0530

    another commit changes

--- scriptcal.js
+++ scriptcal.js
@@ -1,22 +1,32 @@
-const months =[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+const months =[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+let today = new Date();
+let inputget = document.getElementById("date-input");
+inputget.setAttribute('max', `${today.getFullYear()}-${today.getMonth()<10?'0'+(today.getMonth()+1):(today.getMonth()+1)}-${today.getDate()<10?'0'+(today.getDate()-1):(today.getDate()-1)}`)
+
+
 
 function agecalculator(){
-    let today = new Date();
-    let inputdate = new Date(document.getElementById("date-input").value);
+
+ 
+    let inputdate = new Date(inputget.value);
+    
+
     let birthmonth,birthdate, birthyear;
 
     let birthdetail ={
-        date:inputdate.getDate(),
+        date: inputdate.getDate(),
         month:inputdate.getMonth(),
         year:inputdate.getFullYear(),
         
-
     }
+
     let currentyear = today.getFullYear();
     let currentmonth = today.getMonth();
-    let currentdate= today.getDate();
+    let currentdate = today.getDate();
 
     leepcheck(currentyear);
+
+
 
     if (birthdetail.year > currentyear ||
         (birthdetail.month > currentmonth && birthdetail.year == currentyear )
@@ -29,11 +39,11 @@ function agecalculator(){
 
     birthyear = currentyear-birthdetail.year;
     
-    
 
     if( currentmonth >= birthdetail.month){
         birthmonth= currentmonth-birthdetail.month;
     }
+
     else{
         birthyear--;
         birthmonth = 12 + currentmonth- birthdetail.month;
@@ -43,7 +53,7 @@ function agecalculator(){
         birthdate = currentdate-birthdetail.date;
     }else{
         birthmonth--;
-        let days = months[currentmonth - 2];
+        let days = months[currentmonth -1];
         birthdate = days+currentdate -birthdetail.date;
         if(birthmonth<0 ){
             birthmonth=11;
@@ -53,11 +63,19 @@ function agecalculator(){
     if(months>12){
         year++;
     }
-    displayResult(birthdate,birthmonth,birthyear);
+
+
+    if(isNaN(birthdate) || isNaN(birthmonth) || isNaN(birthyear) ){
+        displayResult("-","-","-");
+        alert('Please fill corect date format')
+    }
+    else{
+        displayResult(birthdate,birthmonth,birthyear);
+    }
+    
 }
 function leepcheck(year) {
-    if(year % 4 == 0|| (year % 10 ==0 || 
-        year % 400 ==0) ){
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
             months[1]=29;
         }
         else{
@@ -71,3 +89,6 @@ function displayResult(bdate, bmonth,byear){
     document.getElementById("months").textContent= bmonth;
     document.getElementById("days").textContent= bdate;
 }
+
+
+
